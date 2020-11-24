a65a7c332ad7211eb8272366205cc5562e5f04ec
Tony Costa <tonyblack10@gmail.com>
2020-11-24 01:38:08 -0300

    compondo specifications

--- src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
+++ src/main/java/br/com/alura/spring/data/service/RelatorioFuncionarioDinamico.java
@@ -6,11 +6,16 @@ import br.com.alura.spring.data.specification.SpecificationFuncionario;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
 @Service
 public class RelatorioFuncionarioDinamico {
+
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     private final FuncionarioRepository funcionarioRepository;
 
@@ -22,7 +27,42 @@ public class RelatorioFuncionarioDinamico {
         System.out.println("Digite um nome");
         String nome = scanner.next();
 
+        if (nome.equalsIgnoreCase("NULL")) {
+            nome = null;
+        }
+
+        System.out.println("Digite um cpf");
+        String cpf = scanner.next();
+
+        if (cpf.equalsIgnoreCase("NULL")) {
+            cpf = null;
+        }
+
+        System.out.println("Digite um salario");
+        BigDecimal salario = scanner.nextBigDecimal();
+
+        if (salario.equals(BigDecimal.ZERO)) {
+            salario = null;
+        }
+
+        System.out.println("Digite uma data de contratacao");
+        String data = scanner.next();
+        LocalDate dataContratacao;
+
+        if (data.equalsIgnoreCase("NULL")) {
+            dataContratacao = null;
+        } else {
+            dataContratacao = LocalDate.parse(data, formatter);
+        }
+
         List<Funcionario> list = funcionarioRepository
-                .findAll(Specification.where(SpecificationFuncionario.nome(nome)));
+                .findAll(Specification.where(
+                        SpecificationFuncionario.nome(nome)
+                        .or(SpecificationFuncionario.cpf(cpf))
+                        .or(SpecificationFuncionario.salario(salario))
+                        .or(SpecificationFuncionario.dataContratacao(dataContratacao))
+                ));
+
+        list.forEach(System.out::println);
     }
 }
